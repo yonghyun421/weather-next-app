dbcd09c7ad6f062adc0871fe85bce78aed5e7166
Yonghyun Kim <81367886+yonghyun421@users.noreply.github.com>
2024-07-31 21:51:20 +0900

    feat: weather next app

---
+++ src/app/[location]/page.tsx
@@ -0,0 +1,38 @@
+import HomeButton from "@/components/HomeButton";
+import { getForecast } from "@/utils/getForecast";
+
+type Props = {
+  params: {
+    location: string;
+  };
+  searchParams: {
+    name: string;
+  };
+};
+
+export function generateMetadata({ searchParams }: Props) {
+  return {
+    title: `날씨 앱 - ${searchParams.name}`,
+    description: `${searchParams.name} 날씨를 알려드립니다.`,
+  };
+}
+
+export default async function Detail({ params, searchParams }: Props) {
+  const name = searchParams.name;
+
+  const res = await getForecast(params.location);
+
+  return (
+    <>
+      <h1>{name}의 3일 예보</h1>
+      <ul>
+        {res.forecast.forecastday.map((day) => (
+          <li key={day.date}>
+            {day.date} / {day.day.avgtemp_c}
+          </li>
+        ))}
+      </ul>
+      <HomeButton />
+    </>
+  );
+}
---
+++ src/components/RevalidateButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+type Props = {
+  tag: string;
+};
+
+export default function RevalidateButton({ tag }: Props) {
+  const handleClick = async () => {
+    const res = await fetch("/api/revalidate?tag=" + tag, { method: "POST" });
+
+    const result = await res.json();
+  };
+
+  return <button onClick={handleClick}>캐시 비우기</button>;
+}
